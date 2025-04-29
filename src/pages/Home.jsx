a4071f8db1a56369978244eb7796e3da30a4aa71
KabutoEbi <narenohatehe@gmail.com>
2025-04-29 17:03:40 +0900

    picture etc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,20 @@ const Home = () => {
             <div className="mt-4 text-xl p-4">
                 <p>KabutoEbiのポートフォリオサイトです。</p>
                 <p>作品とか色々載せていきます。</p>
+                <img src="/kabutoebi.png" alt="kabutoebi" className="mx-auto my-10 w-50 rounded-full" />
                 <a href="https://www.nies.go.jp/biodiversity/invasive/DB/detail/70400.html" target="_blank" rel="noopener noreferrer">カブトエビについて→</a>
+            </div>
+            <div className="text-center mt-4">
+                <p className="font-bold text-2xl bg-green-300 border-t-6 border-green-600 p-4">Products</p>
+                <div className="text-xl p-4">
+                    <p><a href="/products">過去の製作物→</a></p>
+                </div>
+            </div>
+            <div className="text-center mt-4">
+                <p className="font-bold text-2xl bg-green-300 border-t-6 border-green-600 p-4">About</p>
+                <div className="text-xl p-4">
+                    <p><a href="/about">詳しい自己紹介→</a></p>
+                </div>
             </div>
             <Articles />
             <Notifications />
