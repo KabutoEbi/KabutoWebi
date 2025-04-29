0e73394470024e090398ea516c85c68f747be4f7
KabutoEbi <narenohatehe@gmail.com>
2025-04-29 14:46:41 +0900

    adjust colors

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Product = ({ Name, description}) => {
     return (
         <div>
-            <p className="font-bold text-2xl bg-green-300 border-t-3 border-green-600 p-3 ">
+            <p className="font-bold text-2xl bg-green-200 border-t-3 border-green-500 p-3 ">
                 {Name}
             </p>
             <p className="text-xl p-2">
