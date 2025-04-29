360e5456ddef1817f5a44346d77e8bc6fc05305c
KabutoEbi <narenohatehe@gmail.com>
2025-04-29 15:26:12 +0900

    adjust color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
     return (
-        <header className="text-center bg-green-300 margin-y-4 p-4 border-b-6 border-green-600">
+        <header className="text-center bg-green-400 margin-y-4 p-4 border-b-6 border-green-600">
             <h1 className="text-5xl font-bold">KabutoWebi</h1>
             <ul className="flex justify-center space-x-5 text-2xl mt-4">
                 <li><a href="/">Home</a></li>
