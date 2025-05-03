78a4ce2cbdaaf7e4c4228b62c243b24a7b1198b3
KabutoEbi <narenohatehe@gmail.com>
2025-05-03 09:36:06 +0900

    装飾を追加

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import "../css/Footer.css";
 
 const Footer = () => {
     return (
         <div className="textcenter mt-4 bg-green-400 border-t-6 border-green-600 p-3">
             <div className="text-xl my-2">
                 <ul className="flex justify-center space-x-4">
-                    <li><a href="https://github.com/KabutoEbi" target="_blank" rel="noopener noreferrer">Github</a></li>
+                    <li className="nav-item-f"><a href="https://github.com/KabutoEbi" target="_blank" rel="noopener noreferrer">Github</a></li>
                     <p>|</p>
-                    <li><a href="https://qiita.com/KabutoEbi" target="_blank" rel="noopener noreferrer">Qiita</a></li>
+                    <li className="nav-item-f"><a href="https://qiita.com/KabutoEbi" target="_blank" rel="noopener noreferrer">Qiita</a></li>
                     <p>|</p>
-                    <li><a href="https://atcoder.jp/users/KabutoEbi" target="_blank" rel="noopener noreferrer">AtCoder</a></li>
+                    <li className="nav-item-f"><a href="https://atcoder.jp/users/KabutoEbi" target="_blank" rel="noopener noreferrer">AtCoder</a></li>
                     <p>|</p>
-                    <li><a href="https://x.com/KabutoEbi_mp3" target="_blank" rel="noopener noreferrer">X</a></li>
+                    <li className="nav-item-f"><a href="https://x.com/KabutoEbi_mp3" target="_blank" rel="noopener noreferrer">X</a></li>
                 </ul>
             </div>
             <p>KabutoEbi + Web = KabutoWebi</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import "../css/Header.css"
 
 const Header = () => {
     return (
         <header className="text-center bg-green-400 margin-y-4 p-4 border-b-6 border-green-600">
             <h1 className="text-5xl font-bold">KabutoWebi</h1>
             <ul className="flex justify-center space-x-5 text-2xl mt-4">
-                <li><a href="/">Home</a></li>
+                <li className="nav-item-h"><a href="/">Home</a></li>
                 <p>|</p>
-                <li><a href="/products">Products</a></li>
+                <li className="nav-item-h"><a href="/products">Products</a></li>
                 <p>|</p>
-                <li><a href="/about">About</a></li>
+                <li className="nav-item-h"><a href="/about">About</a></li>
             </ul>
         </header>
     );
