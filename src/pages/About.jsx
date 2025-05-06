2c2dd5a192cd1b44b0b00458969c98a3e94bea43
KabutoEbi <narenohatehe@gmail.com>
2025-05-06 15:28:18 +0900

    gizyutusutakku

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -44,6 +44,10 @@ const About = () => {
                                 <td className="border border-gray-400 px-4 py-2">ある程度</td>
                             </tr>
                             <tr>
+                                <td className="border border-gray-400 px-4 py-2">Unity/C#</td>
+                                <td className="border border-gray-400 px-4 py-2">ある程度</td>
+                            </tr>
+                            <tr>
                                 <td className="border border-gray-400 px-4 py-2">React</td>
                                 <td className="border border-gray-400 px-4 py-2">ある程度</td>
                             </tr>
