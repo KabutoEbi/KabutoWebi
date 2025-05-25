4239b9afc59f6cb0ea8e2b115126ebdffbcedf51
KabutoEbi <narenohatehe@gmail.com>
2025-05-25 21:30:57 +0900

    add notifications

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -6,6 +6,7 @@ const Notifications = () => {
             <p className="font-bold text-2xl bg-green-300 border-t-2 border-gray-400 p-4 tracking-wide">Notifications</p>
             <div className="mt-4 h-48 overflow-y-scroll rounded ml-5 mr-5">
                 <ul className="text-xl text-left bg-green-100 leading-loose p-5">
+                    <li className="border-b border-green-400">2025年05月25日 一部UIを更新</li>
                     <li className="border-b border-green-400">2025年05月17日 一部UIを更新</li>
                     <li className="border-b border-green-400">2025年05月16日 Productsを更新</li>
                     <li className="border-b border-green-400">2025年04月29日 Productsを更新</li>
