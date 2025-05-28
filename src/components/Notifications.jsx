d7c49e892082b58078af8e743deda809d800cc52
KabutoEbi <narenohatehe@gmail.com>
2025-05-28 22:50:41 +0900

    update ui

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const Notifications = () => {
     return (
         <div className="text-center mt-4">
-            <p className="font-bold text-2xl bg-green-300 border-t-2 border-gray-400 p-4 tracking-wide">Notifications</p>
+            <p className="font-bold text-2xl p-1 border-b-4 border-green-300 inline-block tracking-wide">Notifications</p>
             <div className="mt-4 h-48 overflow-y-scroll rounded ml-5 mr-5">
                 <ul className="text-xl text-left bg-green-100 leading-loose p-5">
+                    <li className="border-b border-green-400">2025年05月28日 一部UIを更新</li>
                     <li className="border-b border-green-400">2025年05月25日 一部UIを更新</li>
                     <li className="border-b border-green-400">2025年05月17日 一部UIを更新</li>
                     <li className="border-b border-green-400">2025年05月16日 Productsを更新</li>
                     <li className="border-b border-green-400">2025年04月29日 Productsを更新</li>
                     <li className="border-b border-green-400">2025年04月29日 Productsを更新</li>
                     <li className="border-b border-green-400">2025年04月29日 サイトを公開</li>
-                    <li className="border-b border-green-400">-</li>
                     <li className="border-b border-green-400">-</li>
                 </ul>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,13 +14,13 @@ const Home = () => {
                 <a href="https://www.nies.go.jp/biodiversity/invasive/DB/detail/70400.html" target="_blank" rel="noopener noreferrer" className="hover:underline">カブトエビについて→</a>
             </div>
             <div className="text-center mt-4">
-                <p className="font-bold text-2xl bg-green-300 border-t-2 border-gray-400 p-4">Products</p>
+                <p className="font-bold text-2xl p-1 border-b-4 border-green-300 inline-block">Products</p>
                 <div className="text-xl p-4">
                     <p><a href="/products" className="hover:underline">過去の製作物→</a></p>
                 </div>
             </div>
             <div className="text-center mt-4">
-                <p className="font-bold text-2xl bg-green-300 border-t-2 border-gray-400 p-4">About</p>
+                <p className="font-bold text-2xl p-1 border-b-4 border-green-300 inline-block">About</p>
                 <div className="text-xl p-4">
                     <p><a href="/about" className="hover:underline">詳しい自己紹介→</a></p>
                 </div>
