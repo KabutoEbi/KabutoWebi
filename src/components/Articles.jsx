07032a5a09ecc597f7f06a6af6cbdbf97588d1e0
KabutoEbi <narenohatehe@gmail.com>
2025-05-30 10:16:01 +0900

    update ui

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -10,7 +10,7 @@ const Articles = () => {
                         href="https://qiita.com/KabutoEbi/items/6b43bd498f3827ea29ab"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="inline-block border-b border-green-400 pb-1 min-w-[35ch] text-center"
+                        className="inline-block border-b border-green-400 pb-1 w-[30ch] text-center"
                     >
                         【高専】Gitプロキシ設定
                     </a>
@@ -20,7 +20,7 @@ const Articles = () => {
                         href="https://qiita.com/KabutoEbi/items/ca4d1f4e2b4ab7b3804e"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="inline-block border-b border-green-400 pb-1 min-w-[27ch] text-center"
+                        className="inline-block border-b border-green-400 pb-1 w-[30ch] text-center"
                     >
                         [Windows]Apple Musicの音が勝手に変わるとき
                     </a>
@@ -30,7 +30,7 @@ const Articles = () => {
                         href="https://qiita.com/KabutoEbi/items/16753cae7f433e0ec461"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="inline-block border-b border-green-400 pb-1 min-w-[35ch] text-center"
+                        className="inline-block border-b border-green-400 pb-1 min-w-[30ch] text-center"
                     >
                         Arduinoで2進数時計を作る
                     </a>
@@ -40,7 +40,7 @@ const Articles = () => {
                         href="https://qiita.com/KabutoEbi/items/25f471faf438e85dcf94"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="inline-block border-b border-green-400 pb-1 min-w-[35ch] text-center"
+                        className="inline-block border-b border-green-400 pb-1 min-w-[30ch] text-center"
                     >
                         高専プロコン35 参加記
                     </a>
