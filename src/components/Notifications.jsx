69627cf41b7bd71ebc8cd892f9682220aca98c88
KabutoEbi <narenohatehe@gmail.com>
2025-05-30 14:47:29 +0900

    update ui

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -4,7 +4,7 @@ const Notifications = () => {
     return (
         <div className="text-center mt-4">
             <p className="font-bold text-2xl p-1 border-b-4 border-green-300 inline-block tracking-wide">Notifications</p>
-            <div className="mt-4 h-48 overflow-y-scroll rounded max-w-xl mx-auto">
+            <div className="mt-4 h-48 overflow-y-scroll rounded mx-auto max-w-fit">
                 <ul className="text-xl text-left bg-green-100 leading-loose p-5">
                     <li className="border-b border-green-400">2025年05月30日 一部UIを更新</li>
                     <li className="border-b border-green-400">2025年05月28日 一部UIを更新</li>
